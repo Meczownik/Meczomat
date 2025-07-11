13ee38e44255c4de702f65d788fddc03b4013bc4
Jakub-Kuzakowski <kubabibu2205@gmail.com>
2025-07-11 02:10:57 +0200

    Zaktualizowanie mapki

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,26 +1,42 @@
 $(document).ready(function(){
-
     $(".info-box").hide();
-    
-    $("#mazowieckie").click(function(e){
-        e.stopPropagation();
-        $(".info-box").hide();
-        $("#info-mazowieckie").toggle();
-    });
 
-    $("#pomorskie").click(function(e){
-        e.stopPropagation();
-        $(".info-box").hide();
-        $("#info-pomorskie").toggle();
-    });
+    // Tablica z parami: [przycisk, box do pokazania]
+    const pairs = [
+        ["#mazowieckie", "#info-mazowieckie"],
 
-    $("#malopolskie").click(function(e){
-        e.stopPropagation();
-        $(".info-box").hide();
-        $("#info-malopolskie").toggle();
-    });
+        //Pomorskie
 
-    $(document).click(function(){
-        $(".info-box").hide();
+        ["#pomorskie", "#info-pomorskie"],
+        ["#a_gdansk", "#A-klasa_Gdansk"],
+        ["a_malbork", "#A-klasa_Malbork"],
+        ["#b_gdansk", "#B-klasa_Gdansk"],
+        ["#b_malbork", "#B-klasa_Malbork"],
+
+        //Małopolskie
+
+        ["#malopolskie", "#info-malopolskie"],
+        ["#a_wadowice", "#A-klasa_Wadowice"],
+        ["#b_limanowa", "#B-klasa_Limanowa"],
+        ["#b_wadowice", "#B-klasa_Wadowice"],
+
+        //Dolnośląskie
+
+        ["#dolnoslaskie", "#info-dolnoslaskie"],
+        ["#a_wroclaw", "#A-klasa_Wroclaw"],
+        ["#a_walbrzych", "#A-klasa_Walbrzych"],
+        ["#a_legnica", "#A-klasa_Legnica"],
+        ["#a_jelenia_gora", "#A-klasa_Jelenia_Gora"],
+        ["#b_wroclaw", "#B-klasa_Wroclaw"],
+        ["#b_walbrzych", "#B-klasa_Walbrzych"],
+        ["#b_legnica", "#B-klasa_Legnica"],
+        ["#b_jelenia_gora", "#B-klasa_Jelenia_Gora"]
+    ];
+
+    pairs.forEach(([btn, box]) => {
+        $(btn).click(function(e){
+            e.stopPropagation();
+            $(box).slideToggle();
+        });
     });
 });
